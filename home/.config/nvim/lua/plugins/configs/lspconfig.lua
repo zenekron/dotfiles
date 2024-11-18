c3e478b2c64de05acd6c9bc44439c0682ddeafeb
zenekron <zenekron@gmail.com>
2024-11-18 09:54:04 +0100

    fix(neovim): suppress "server cancelled the request" messages

--- home/.config/nvim/lua/plugins/configs/lspconfig.lua
+++ home/.config/nvim/lua/plugins/configs/lspconfig.lua
@@ -63,6 +63,16 @@ M.setup = function()
   vim.keymap.set("n", "<leader>s", vim.lsp.buf.workspace_symbol, kopts)
 
 
+  -- HACK: https://github.com/neovim/neovim/issues/30985
+  for _, method in ipairs { "textDocument/diagnostic", "workspace/diagnostic" } do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+      if err ~= nil and err.code == -32802 then return end
+      return default_diagnostic_handler(err, result, context, config)
+    end
+  end
+
+
   --
   -- nvim-cmp
   --
