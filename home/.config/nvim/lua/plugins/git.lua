d2441b0a518235d53e8912a552a0b83beabeee7d
zenekron <zenekron@gmail.com>
2025-03-26 20:23:36 +0100

    feat(neovim): configure gitsigns keybinds

--- home/.config/nvim/lua/plugins/git.lua
+++ home/.config/nvim/lua/plugins/git.lua
@@ -8,6 +8,124 @@ return {
 
 		opts = {},
 
+		lazy = false,
+		keys = {
+			{
+				"<leader>hb",
+				function()
+					require("gitsigns").blame_line()
+				end,
+				desc = "git: [b]lame line",
+			},
+			{
+				"<leader>hB",
+				"<cmd>Gitsigns blame<cr>",
+				desc = "git: toggle [B]lame",
+			},
+
+			{
+				"<leader>hp",
+				function()
+					require("gitsigns").preview_hunk()
+				end,
+				desc = "git: [p]review hunk",
+			},
+			{
+				"<leader>hP",
+				function()
+					require("gitsigns").preview_hunk_inline()
+				end,
+				desc = "git: [P]review hunk inline",
+			},
+
+			{
+				"<leader>hd",
+				function()
+					require("gitsigns").diffthis()
+				end,
+				desc = "git: [d]iff against index",
+			},
+			{
+				"<leader>hD",
+				function()
+					require("gitsigns").diffthis("@")
+				end,
+				desc = "git: [D]iff against last commit",
+			},
+
+			-- navigation
+			{
+				"]c",
+				function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						require("gitsigns").nav_hunk("next")
+					end
+				end,
+				desc = "git: jump to next [c]hange",
+			},
+			{
+				"[c",
+				function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						require("gitsigns").nav_hunk("prev")
+					end
+				end,
+				desc = "git: jump to previous [c]hange",
+			},
+
+			-- hunk actions
+			{
+				"<leader>hs",
+				function()
+					require("gitsigns").stage_hunk()
+				end,
+				desc = "git: [s]tage hunk",
+			},
+			{
+				"<leader>hs",
+				function()
+					require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end,
+				mode = "v",
+				desc = "git: [s]tage hunk",
+			},
+			{
+				"<leader>hr",
+				function()
+					require("gitsigns").reset_hunk()
+				end,
+				desc = "git: [r]eset hunk",
+			},
+			{
+				"<leader>hr",
+				function()
+					require("gitsigns").reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end,
+				mode = "v",
+				desc = "git: [r]eset hunk",
+			},
+
+			-- buffer actions
+			{
+				"<leader>hS",
+				function()
+					require("gitsigns").stage_buffer()
+				end,
+				desc = "git: [S]tage buffer",
+			},
+			{
+				"<leader>hR",
+				function()
+					require("gitsigns").stage_buffer()
+				end,
+				desc = "git: [R]eset buffer",
+			},
+		},
+
 		version = "*",
 	},
 
