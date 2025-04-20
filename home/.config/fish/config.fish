b7fe21833f9b28aa6044093774a02a99f54c567d
zenekron <zenekron@gmail.com>
2025-04-20 17:30:11 +0200

    feat(fish): automatically start sway on login

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -15,3 +15,10 @@ fish_add_path "$HOME/.dotfiles/bin"
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+if status is-login
+	if test -z "$WAYLAND_DISPLAY" -a $XDG_VTNR -eq 1
+		exec wayland-run.sh sway
+		# exec wayland-run.sh Hyprland
+	end
+end
