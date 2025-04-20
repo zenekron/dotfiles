a591dcccbf7f0d410007fc3415c5a8717617d40e
zenekron <zenekron@gmail.com>
2025-04-20 17:29:23 +0200

    refactor(fish): configure user paths in config.fish to avoid hard-coded user paths

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,12 @@
+# https://wiki.archlinux.org/title/XDG_Base_Directory
+set -x XDG_CACHE_HOME "$HOME/.cache"
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x XDG_DATA_HOME "$HOME/.local/share"
+set -x XDG_STATE_HOME "$HOME/.local/state"
+
+# update path
+fish_add_path "$HOME/.dotfiles/bin"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
