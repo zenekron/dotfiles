dd78f694f38e8e6dfad3644e9db057285e9274ab
zenekron <zenekron@gmail.com>
2025-04-20 17:34:30 +0200

    chore(nix): initialize flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # nix
+            alejandra
+            nil
+          ];
+        };
+      }
+    );
+}
